1d841ee43a6776a178960110af9283d8bf90281b
Milarrr <43506042+Milarrr@users.noreply.github.com>
2018-10-05 10:19:26 +0700

    Update komentar.php
    MILAROHMAT_6701170136

--- komentar.php
+++ komentar.php
@@ -1,3 +1,13 @@
 <?php
-
+<h1>Hitung Jumlah kata<h1>
+<form id="form2" name="form2" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   <input type="text" name='kata'><br/><br/>
+   <input type="submit" name="hitungkata" id="hitungkata" value="hitung" />
+</form>
+<?php
+   if(isset($_POST['hitungkata'])){
+      $hasil=explode(" ", $_POST['kata']);
+      echo "sekitar " . count($hasil) . " kata";
+   }
+?>
 ?>
