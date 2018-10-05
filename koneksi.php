bb1d7374a91a2dac38d5d1c4ae78256830b7f475
Milarrr <43506042+Milarrr@users.noreply.github.com>
2018-10-05 10:20:17 +0700

    Update koneksi.php
    MILAROHMAT_6701170136

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,26 @@
 <?php
+$koneksi = new mysqli("localhost", "root", "", "daft");
 
+  			if (isset($_POST['submit'])) {
+				$koneksi = new mysqli("localhost", "root", "", "daft");
+
+				if ($koneksi) {
+					$nim = $_POST['Nim'];
+					$nama = $_POST['Nama'];
+					$email = $_POST['Email'];
+					if(strlen($nim)==10 && ($nama)<25){
+					
+					$sql = $koneksi->query("
+						INSERT INTO `mahasiswa` (`Nim`, `Nama`, `Email`)
+						VALUES ('$nim', '$nama', '$email')
+										   ");
+					echo "REGISTRASI SUKSES<br>";
+				
+				}
+				else{
+					echo "REGISTRASI GAGAL";
+				}
+				}else{
+				}
+  			}
 ?>
