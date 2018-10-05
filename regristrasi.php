10457aee1b107e09c8125a4a92bf9f60a833c052
Milarrr <43506042+Milarrr@users.noreply.github.com>
2018-10-05 10:21:50 +0700

    Update regristrasi.php
    MILAROHMAT_6701170136

--- regristrasi.php
+++ regristrasi.php
@@ -4,8 +4,12 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
+<form method="post" action="prosesregist.php">
+		Nim  : <input type="number" name="Nim" maxlength=10 /></br>
+		Nama : <input type="text" name="Nama" maxlength="" =25 /></br>
+		Email: <input type="email" name="Email"> </br></br>
 
-
-
+		<input type="submit" name="submit" value="regist"/>
+	</form>
 </body>
 </html>
